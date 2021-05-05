7546ff675870200ee2bcd442b33e9d7b60a48890
Sukma Wijaya Saputra <coklatpanas83@gmail.com>
2021-05-05 14:55:10 +0700

    My first commit

---
+++ hari02/latihan01.py
@@ -0,0 +1,21 @@
+# pelatihan hari 2
+
+# or create a new repository on the command line
+#echo "# pelatihanKemenPora" >> README.md
+#git init
+#git add README.md
+#git commit -m "first commit"
+#git branch -M main
+#git remote add origin https://github.com/ombak/pelatihanKemenPora.git
+#git push -u origin main
+
+# or push an existing repository from the command line
+#git remote add origin https://github.com/ombak/pelatihanKemenPora.git
+#git branch -M main
+#git push -u origin main
+
+
+# not !=  
+
+#print(not True)
+print(not True)
